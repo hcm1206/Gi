10bc5f6dc22d5ccec1af3ad8658a9568de492b07
컴퓨터\kim-jin-young <wlsdud11457@gmail.com>
2023-05-11 17:50:44 +0900

    main.html 과 express를 연동 시켜서 localhost:5000을 입력시 logo.png가 나오도록 설정함

--- gi_server/index.js
+++ gi_server/index.js
@@ -1,13 +1,28 @@
-// index.js
+const express = require('express');
+const app = express();
 
-const express = require('express')	// 모듈 추출
-const app = express()			// 서버 생성
-const port = 5000			// 서버주소(port) 지정
+// 'GI' 폴더를 정적 파일 경로로 지정합니다.
+app.use('/main_images', express.static(__dirname + '/main_images'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 라우터에 GET 요청 핸들러를 등록합니다.
+app.get('/', function(req, res) {
+  // main.html 파일을 렌더링합니다.
+  res.sendFile(__dirname + '/main.html');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/main_images', function(req, res) {
+  fs.readFile('./main_images/logo.png', function(err, data) {
+    if (err) {
+      console.log(err);
+      res.status(404).end();
+    } else {
+      res.writeHead(200, {'Content-Type': 'image/png'});
+      res.end(data);
+    }
+  });
+});
+
+// 서버를 5000번 포트로 엽니다.
+app.listen(5000, function() {
+  console.log('서버가 시작되었습니다.');
+});
