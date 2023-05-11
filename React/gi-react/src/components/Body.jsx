343ae485a96f2fe60871176416cbcd3d1aa073db
hcm1206 <hcm1206@naver.com>
2023-05-11 17:50:42 +0900

    리액트 프론트페이지 / 회원가입페이지 / 503 에러 페이지 테스트 구현

--- React/gi-react/src/components/Body.jsx
+++ React/gi-react/src/components/Body.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import FrontPage from './FrontPage/FrontPage';
+import SignUpPage from "./SignUpPage.jsx/SignUpPage";
+import ErrorPage from "./ErrorPage/ErrorPage";
+
+// 라우팅 처리 예정
 
 function Body() {
     return (
         <BodyWrapper>
             <FrontPage/>
+            {/* <SignUpPage/> */}
+            {/* <ErrorPage/> */}
         </BodyWrapper>
     )
 }
 
+// 높이는 나중에 수정
 const BodyWrapper = styled.div`
     width: 100%;
-    height: 1000px;
     background-color: #FAFFFF;
     margin: 0;
     padding: 0;
